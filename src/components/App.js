7c08117ce7e1590d233a012cbf5078424b3db28c
orvalho <rasa396@gmail.com>
2019-08-13 23:36:50 +0300

    add 'not found page'

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
 import '../scss/App.scss';
 
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Header from './Header';
 import Home from './Home';
 import NameList from './NameList';
 import NameBox from './NameBox';
 import Profile from './Profile';
+import NotFound from './NotFound';
 
-export default() => {
-  return (<div className="app">
+export default() => (
+  <div className="app">
     <BrowserRouter>
       <Route path="/" component={Header}/>
-      <Route path="/" exact={true} component={Home}/>
-      <Route path="/names/girls" component={NameList}/>
-      <Route path="/names/boys" component={NameList}/>
-      <Route path="/names/new" component={NameBox}/>
-      <Route path="/profile" component={Profile}/>
+      <Switch>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="/names/girls" component={NameList}/>
+        <Route path="/names/boys" component={NameList}/>
+        <Route path="/names/new" component={NameBox}/>
+        <Route path="/profile" component={Profile}/>
+        <Route component={NotFound}/>
+      </Switch>
     </BrowserRouter>
   </div>);
-};
+
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import '../scss/NotFound.scss';
+
+import React from 'react';
+
+export default () => (
+    <div className="not-found">
+        <h1>Page Not Found!!!</h1>
+    </div>
+);
